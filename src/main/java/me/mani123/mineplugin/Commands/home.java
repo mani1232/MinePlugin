63228e1c6ed5f6ada3ba0d51742fade922c60d3b
mani123 <perederkoura@gmail.com>
2021-06-17 00:44:02 +0300

    + tab complete

--- src/main/java/me/mani123/mineplugin/Commands/home.java
+++ src/main/java/me/mani123/mineplugin/Commands/home.java
@@ -4,19 +4,18 @@ import me.mani123.mineplugin.MinePlugin;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.io.Console;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import static org.bukkit.Bukkit.getServer;
 
-public class home implements CommandExecutor {
+public class home implements CommandExecutor, TabCompleter {
 
     MinePlugin plugin;
 
@@ -27,50 +26,50 @@ public class home implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command,
                              @NotNull String label, @NotNull String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            Location location = player.getLocation();
-            World world = player.getWorld();
-            if (plugin.getConfig().getBoolean("Enable")) {
-                if (args.length == 1 && args[0].equalsIgnoreCase("set")) {
-                    if (plugin.getConfig().isConfigurationSection("savedLocations." + player.getName())) {
-                        player.sendMessage(ChatColor.YELLOW + "Your home location set to:" + ChatColor.AQUA
-                                + Math.round(location.getX()) + "," + Math.round(location.getY()) +
-                                "," + Math.round(location.getZ()));
-                        saveToConfig(location, player, world);
-                    } else {
-                        saveToConfig(location, player, world);
-                        player.sendMessage(ChatColor.GOLD + "You set location the home " + ChatColor.AQUA +
-                                "(" + Math.round(location.getX()) + "," + Math.round(location.getY()) +
-                                "," + Math.round(location.getZ()) + ")");
+            if (sender instanceof Player && args.length != 0) {
+                Player player = (Player) sender;
+                Location location = player.getLocation();
+                World world = player.getWorld();
+                if (plugin.getConfig().getBoolean("Enable")) {
+                    if (args.length == 1 && args[0].equalsIgnoreCase("set")) {
+                        if (plugin.getConfig().isConfigurationSection("savedLocations." + player.getName())) {
+                            player.sendMessage(ChatColor.YELLOW + "Your home location set to:" + ChatColor.AQUA
+                                    + Math.round(location.getX()) + "," + Math.round(location.getY()) +
+                                    "," + Math.round(location.getZ()));
+                            saveToConfig(location, player, world);
+                        } else {
+                            saveToConfig(location, player, world);
+                            player.sendMessage(ChatColor.GOLD + "You set location the home " + ChatColor.AQUA +
+                                    "(" + Math.round(location.getX()) + "," + Math.round(location.getY()) +
+                                    "," + Math.round(location.getZ()) + ")");
+                        }
+                    } else if (args.length == 1 && args[0].equalsIgnoreCase("tp")) {
+                        if (plugin.getConfig().isConfigurationSection("savedLocations." + player.getName())) {
+                            Location go_home = new Location(getServer().getWorld(Objects.requireNonNull(plugin.getConfig()
+                                    .getString("savedLocations." + player.getName() + ".world")))
+                                    , plugin.getConfig().getDouble("savedLocations." + player.getName() + ".x")
+                                    , plugin.getConfig().getDouble("savedLocations." + player.getName() + ".y")
+                                    , plugin.getConfig().getDouble("savedLocations." + player.getName() + ".z"));
+                            player.teleport(go_home);
+                            player.sendMessage(ChatColor.GREEN + "You teleported to home location");
+                        }
                     }
-                } else if (args.length == 1 && args[0].equalsIgnoreCase("tp")) {
-                    if (plugin.getConfig().isConfigurationSection("savedLocations." + player.getName())) {
-                        Location go_home = new Location(getServer().getWorld(Objects.requireNonNull(plugin.getConfig()
-                                .getString("savedLocations." + player.getName() + ".world")))
-                                , plugin.getConfig().getDouble("savedLocations." + player.getName() + ".x")
-                                , plugin.getConfig().getDouble("savedLocations." + player.getName() + ".y")
-                                , plugin.getConfig().getDouble("savedLocations." + player.getName() + ".z"));
-                        player.teleport(go_home);
-                        player.sendMessage(ChatColor.GREEN + "You teleported to home location");
-                    }
+                } else {
+                    notCommand(player);
                 }
-            } else {
-                notCommand(player);
+
+            } else if (sender instanceof ConsoleCommandSender && !args[0].equalsIgnoreCase("reload")) {
+                System.out.println(ChatColor.RED + "This command work only in game!");
             }
 
-        }else if (!(sender instanceof ConsoleCommandSender)) { System.out.println(ChatColor.RED + "This command work only in game!");}
-
-        if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-            if (sender instanceof Player) {
-                plugin.reloadConfig();
-                sender.sendMessage(ChatColor.RED + "Reloading the plugin via the game");
-            } else if (sender instanceof ConsoleCommandSender){
-                System.out.println(ChatColor.RED + "Reloading the plugin via the console");
-            } else {
-                System.out.println(ChatColor.RED + "Plugin not reloaded");
+            if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+                if (!(sender instanceof ConsoleCommandSender)) {
+                    plugin.reloadConfig();
+                    sender.sendMessage(ChatColor.RED + "Reloading the plugin via the game");
+                } else {
+                    System.out.println(ChatColor.RED + "Reloading the plugin via the console");
+                }
             }
-        }
         return true;
     }
 
@@ -88,5 +87,18 @@ public class home implements CommandExecutor {
         plugin.getConfig().set("savedLocations." + player.getName() + ".yaw", location.getYaw());
         plugin.getConfig().set("savedLocations." + player.getName() + ".world", world.getName());
         plugin.saveConfig();
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender,
+                                                @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        if (args.length == 1){
+            List<String> arguments = new ArrayList<>();
+            arguments.add("tp");
+            arguments.add("set");
+            arguments.add("reload");
+            return arguments;
+        }
+        return null;
     }
 }
